bbba0f3f954d806bac85f7328f4214848ce1378b
Mohd Shahid <sid@github.com>
2020-10-14 15:35:56 +0530

    use extends in pug

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
+const viewRouter = require('./routes/viewRoutes');
 
 const app = express();
 
@@ -71,12 +72,7 @@ app.use((req, res, next) => {
 });
 
 // 3) ROUTES
-app.get('/', (req, res) => {
-	res.status(200).render('base', {
-		title: 'Natours | Exciting tours for adventurous people',
-	});
-});
-
+app.use('/', viewRouter);
 app.use('/api/v1/reviews', reviewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
