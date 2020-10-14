434177bd04157fefda783f165affe0224fc83baf
Mohd Shahid <sid@github.com>
2020-10-14 18:55:00 +0530

    make overview view

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,8 +1,19 @@
-exports.getOverview = (req, res) => {
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getOverview = catchAsync(async (req, res) => {
+	// get all the tour data from collection
+	const tours = await Tour.find();
+
+	// build template
+
+	// render the template using the tours data
+
 	res.status(200).render('overview', {
 		title: 'All Tours',
+		tours,
 	});
-};
+});
 
 exports.getTour = (req, res) => {
 	res.status(200).render('tour', {
