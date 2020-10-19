1e8aac2055939491a27d32328e80dd98c483b238
Mohd Shahid <sid@github.com>
2020-10-19 09:48:48 +0530

    alert messages

--- public/js/index.js
+++ public/js/index.js
@@ -2,9 +2,13 @@
 import '@babel/polyfill';
 import { login } from './login';
 
-document.querySelector('.form').addEventListener('submit', (e) => {
-	e.preventDefault();
-	const email = document.getElementById('email').value;
-	const password = document.getElementById('password').value;
-	login(email, password);
-});
+const loginForm = document.querySelector('.form');
+
+if (loginForm) {
+	loginForm.addEventListener('submit', (e) => {
+		e.preventDefault();
+		const email = document.getElementById('email').value;
+		const password = document.getElementById('password').value;
+		login(email, password);
+	});
+}
