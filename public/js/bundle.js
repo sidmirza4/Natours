59a14e11873396199686f1ad56d8548a27078498
Mohd Shahid <sid@github.com>
2020-10-21 09:11:47 +0530

    complete file file upload using multer

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -8996,7 +8996,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "39669" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "45447" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
