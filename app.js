c6e22865d3ed176715341d51cede9d2c6a76ebec
Mohd Shahid <sid@github.com>
2020-10-22 13:21:48 +0530

    implement CORS

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -26,6 +27,12 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 // 1) GLOBAL MIDDLEWARES
+// implememnt CORS
+// for simple requests (get , post)
+app.use(cors());
+
+// for non-simple requests (put, putch, delete)
+app.options('*', cors());
 
 // serving static files
 // app.use(express.static(`${__dirname}/public`));
