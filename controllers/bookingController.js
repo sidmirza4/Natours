2aed553be99f3bd39f07038c2f4a65cb74adf60d
Mohd Shahid <sid@github.com>
2020-10-22 10:42:07 +0530

    make bookings routes

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -3,6 +3,7 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const Booking = require('../models/bookingModel');
 const Tour = require('../models/tourModel');
 const catchAsync = require('../utils/catchAsync');
+const factory = require('../controllers/handlerFactory');
 
 exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 	// get currently booked tour
@@ -48,3 +49,9 @@ exports.createBookingCheckout = catchAsync(async (req, res, next) => {
 
 	res.redirect(req.originalUrl.split('?')[0]);
 });
+
+exports.createBooking = factory.createOne(Booking);
+exports.getBooking = factory.getOne(Booking);
+exports.getAllBooking = factory.getAll(Booking);
+exports.updateBooking = factory.updateOne(Booking);
+exports.deleteBooking = factory.deleteOne(Booking);
--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -1,9 +1,29 @@
 const express = require('express');
-const { protect } = require('./../controllers/authController');
-const { getCheckoutSession } = require('./../controllers/bookingController');
+const { protect, restrictTo } = require('./../controllers/authController');
+const {
+	getCheckoutSession,
+	getAllBooking,
+	createBooking,
+	getBooking,
+	updateBooking,
+	deleteBooking,
+} = require('./../controllers/bookingController');
 
 const router = express.Router({ mergeParams: true });
 
+router.use(protect);
 router.get('/checkout-session/:tourID', protect, getCheckoutSession);
+
+router.use(restrictTo('admin', 'lead-guide'));
+router
+	.route('/')
+	.get(getAllBooking)
+	.post(createBooking);
+
+router
+	.route('/:id')
+	.get(getBooking)
+	.patch(updateBooking)
+	.delete(deleteBooking);
 
 module.exports = router;
