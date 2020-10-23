1118ee2ccbee34bac7d03f5fb92dfd831d1863e6
Mohd Shahid <sid@github.com>
2020-10-23 09:50:12 +0530

    refactor

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -3,17 +3,12 @@ const viewsController = require('../controllers/viewsController');
 const authController = require('../controllers/authController');
 
 const router = express.Router();
-
 router.use(viewsController.alerts);
-
 router.get('/', authController.isLoggedIn, viewsController.getOverview);
-
 router.get('/tour/:slug', authController.isLoggedIn, viewsController.getTour);
 router.get('/login', authController.isLoggedIn, viewsController.getLoginForm);
 router.get('/me', authController.protect, viewsController.getAccount);
-
 router.get('/my-tours', authController.protect, viewsController.getMyTours);
-
 router.post(
 	'/submit-user-data',
 	authController.protect,
